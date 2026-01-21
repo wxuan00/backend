b9bf64892387dca2ea51184796ced306d606fc1d
gwx <wx.weixuangoh@gmail.com>
2026-01-21 22:54:30 +0800

    initial commit

---
+++ src/main/java/com/msp/MspApplication.java
@@ -0,0 +1,13 @@
+package com.msp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msp/MspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
